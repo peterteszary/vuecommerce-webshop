ddabb5b7dc8d99157942eb712eb7408d7fbe8eb3
Teszáry Péter <peterteszary@gmail.com>
2024-02-28 20:20:47 +0100

    Clean Up and Catalogue View added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vuecommerce-webshop
 This is my Vue based WebShop
+
+https://www.youtube.com/watch?v=NUDBj7uJygI
---
+++ vuecommecre-webshop/src/views/Catalog.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Catalog</h1>
+</template>
+
+<script>
+    import { defineComponent  } from 'vue';
+    export default defineComponent({
+        name: 'CatalogView'
+    })
+</script>
